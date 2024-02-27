be02dfab9d9744cc6a0c41f3d3efe8248ec270c7
Milan Jaros <milan.jaros@vsb.cz>
2024-02-27 17:14:09 +0100

    add check haystack lib to addon

--- viewer/blender/addons/hayStack/haystack_render.py
+++ viewer/blender/addons/hayStack/haystack_render.py
@@ -42,17 +42,7 @@ from mathutils import Matrix
 from ctypes import Array, cdll, c_void_p, c_char, c_char_p, c_int, c_int32, c_uint32, c_float, c_bool, c_ulong, POINTER
 
 from . import haystack_pref
-#####################################################################################################################
-bl_info = {
-    "name": "hayStack",
-    "author": "Milan Jaros, Ingo Wald",
-    "description": "",
-    "blender": (3, 3, 0),
-    "version": (0, 0, 1),
-    "location": "",
-    "warning": "",
-    "category": "Render"
-}
+
 #####################################################################################################################
 if platform.system() == 'Windows':
     gl = ctypes.windll.opengl32
@@ -80,59 +70,62 @@ else:
         "Libraries not available for this platform: " + platform.system())
 
 #####################################################################################################################
+try:
+    # Load library
+    _renderengine_dll_name = os.path.join(
+        os.path.dirname(__file__), _renderengine_dll_name)
+    _renderengine_dll = cdll.LoadLibrary(_renderengine_dll_name)
 
-# Load library
-_renderengine_dll_name = os.path.join(
-    os.path.dirname(__file__), _renderengine_dll_name)
-_renderengine_dll = cdll.LoadLibrary(_renderengine_dll_name)
+    #####################################################################################################################
 
-#####################################################################################################################
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD resize(int width, int height);
+    _renderengine_dll.resize.argtypes = [c_int32, c_int32]
 
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD resize(int width, int height);
-_renderengine_dll.resize.argtypes = [c_int32, c_int32]
+    # HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD recv_pixels_data();
+    # HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD send_cam_data();
+    _renderengine_dll.recv_pixels_data.restype = c_int32
+    _renderengine_dll.send_cam_data.restype = c_int32
 
-# HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD recv_pixels_data();
-# HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD send_cam_data();
-_renderengine_dll.recv_pixels_data.restype = c_int32
-_renderengine_dll.send_cam_data.restype = c_int32
+    #HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_timestep(int timestep);
+    _renderengine_dll.set_timestep.argtypes = [c_int32]
 
-#HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_timestep(int timestep);
-_renderengine_dll.set_timestep.argtypes = [c_int32]
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD client_init(const char* server, int port_cam, int port_data, int w, int h, int step_samples, const char* filename);
+    _renderengine_dll.client_init.argtypes = [
+        c_char_p, c_int32, c_int32, c_int32, c_int32, c_int32, c_char_p]
 
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD client_init(const char* server, int port_cam, int port_data, int w, int h, int step_samples, const char* filename);
-_renderengine_dll.client_init.argtypes = [
-    c_char_p, c_int32, c_int32, c_int32, c_int32, c_int32, c_char_p]
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD client_close_connection();
 
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD client_close_connection();
-
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_camera(void* view_martix,
-# 	float lens,
-# 	float nearclip,
-# 	float farclip,
-# 	float sensor_width,
-# 	float sensor_height,
-# 	int sensor_fit,
-# 	float view_camera_zoom,
-# 	float view_camera_offset0,
-# 	float view_camera_offset1,
-# 	int use_view_camera,
-# 	float shift_x,
-# 	float shift_y);
-_renderengine_dll.set_camera.argtypes = [c_void_p, c_float, c_float, c_float,
-                                         c_float, c_float, c_int, c_float, c_float, c_float, c_int, c_float, c_float]
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_camera(void* view_martix,
+    # 	float lens,
+    # 	float nearclip,
+    # 	float farclip,
+    # 	float sensor_width,
+    # 	float sensor_height,
+    # 	int sensor_fit,
+    # 	float view_camera_zoom,
+    # 	float view_camera_offset0,
+    # 	float view_camera_offset1,
+    # 	int use_view_camera,
+    # 	float shift_x,
+    # 	float shift_y);
+    _renderengine_dll.set_camera.argtypes = [c_void_p, c_float, c_float, c_float,
+                                            c_float, c_float, c_int, c_float, c_float, c_float, c_int, c_float, c_float]
 
 
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD get_pixels(void* pixels);
-_renderengine_dll.get_pixels.argtypes = [c_void_p]
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD get_pixels(void* pixels);
+    _renderengine_dll.get_pixels.argtypes = [c_void_p]
 
-# HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD get_samples();
-# HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD get_current_samples();
-_renderengine_dll.get_samples.restype = c_int32
-_renderengine_dll.get_current_samples.restype = c_int32
+    # HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD get_samples();
+    # HAYSTACK_EXPORT_DLL int HAYSTACK_EXPORT_STD get_current_samples();
+    _renderengine_dll.get_samples.restype = c_int32
+    _renderengine_dll.get_current_samples.restype = c_int32
 
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD reset();
-# HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_haystack_data(void* values, int size);
-_renderengine_dll.set_haystack_data.argtypes = [c_void_p, c_int32]
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD reset();
+    # HAYSTACK_EXPORT_DLL void HAYSTACK_EXPORT_STD set_haystack_data(void* values, int size);
+    _renderengine_dll.set_haystack_data.argtypes = [c_void_p, c_int32]
+
+except:
+    print("Missing: ", _renderengine_dll_name)
 
 #####################################################################################################################
 
